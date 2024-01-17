b52914a555503eeaecebe7c3ed821abd7076fed6
Jack Lingle <captainjack0818@gmail.com>
2024-01-17 17:52:34 -0500

    add imave as seperate component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import {Image, StyleSheet, Text, View} from 'react-native';
+import ImageViewer from "./components/ImageViewer";
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage}/>
       </View>
       <StatusBar style="auto" />
     </View>
