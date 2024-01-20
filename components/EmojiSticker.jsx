69a36c0f830d7fa7fe6b04c662c756bcf4f4bd3f
Jack Lingle <captainjack0818@gmail.com>
2024-01-20 11:15:51 -0500

    add emoji resize doubletap gesture;

--- components/EmojiSticker.jsx
+++ components/EmojiSticker.jsx
@@ -1,9 +1,29 @@
 import { Image, View } from "react-native";
+import Animated, {useAnimatedStyle, useSharedValue, withSpring} from 'react-native-reanimated';
+import {Gesture, GestureDetector} from "react-native-gesture-handler";
 
 export default function EmojiSticker ({ imageSize, stickerSource }) {
+    const scaledImage = useSharedValue(imageSize);
+
+    const doubleTap = Gesture.Tap()
+        .numberOfTaps(2)
+        .onStart(() => {
+            if(scaledImage.value !== imageSize * 2) {
+                scaledImage.value = scaledImage.value * 2;
+            }
+        });
+
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaledImage.value),
+            height: withSpring(scaledImage.value),
+        }
+    })
     return (
         <View style={{ top: -350 }}>
-            <Image source={stickerSource} resizeMode={'contain'} style={{ width: imageSize, height: imageSize }} />
+            <GestureDetector gesture={doubleTap}>
+                <Animated.Image source={stickerSource} resizeMode={'contain'} style={[imageStyle, { width: imageSize, height: imageSize }]} />
+            </GestureDetector>
         </View>
     )
 }
