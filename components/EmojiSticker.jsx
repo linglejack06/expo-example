cbd9534defc0c46128f41bca24b5c0a317b9c712
Jack Lingle <captainjack0818@gmail.com>
2024-01-20 11:21:30 -0500

    add drag gesture and shrink

--- components/EmojiSticker.jsx
+++ components/EmojiSticker.jsx
@@ -4,14 +4,24 @@ import {Gesture, GestureDetector} from "react-native-gesture-handler";
 
 export default function EmojiSticker ({ imageSize, stickerSource }) {
     const scaledImage = useSharedValue(imageSize);
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
 
     const doubleTap = Gesture.Tap()
         .numberOfTaps(2)
         .onStart(() => {
             if(scaledImage.value !== imageSize * 2) {
                 scaledImage.value = scaledImage.value * 2;
+            } else {
+                scaledImage.value = imageSize;
             }
         });
+
+    const drag = Gesture.Pan()
+        .onChange((event) => {
+            translateX.value += event.changeX;
+            translateY.value += event.changeY;
+        })
 
     const imageStyle = useAnimatedStyle(() => {
         return {
@@ -19,11 +29,26 @@ export default function EmojiSticker ({ imageSize, stickerSource }) {
             height: withSpring(scaledImage.value),
         }
     })
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: translateX.value
+                },
+                {
+                    translateY: translateY.value
+                }
+            ]
+        }
+    })
     return (
-        <View style={{ top: -350 }}>
-            <GestureDetector gesture={doubleTap}>
-                <Animated.Image source={stickerSource} resizeMode={'contain'} style={[imageStyle, { width: imageSize, height: imageSize }]} />
-            </GestureDetector>
-        </View>
+        <GestureDetector gesture={drag}>
+            <Animated.View style={[containerStyle, { top: -350 }]}>
+                <GestureDetector gesture={doubleTap}>
+                    <Animated.Image source={stickerSource} resizeMode={'contain'} style={[imageStyle, { width: imageSize, height: imageSize }]} />
+                </GestureDetector>
+            </Animated.View>
+        </GestureDetector>
     )
 }
